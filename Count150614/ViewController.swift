1fe220ba891b58e1f417875646813aa75de80f98
Ken Tominaga <ken.for.develop@gmail.com>
2015-06-14 10:11:34 +0900

    Initial Commit

---
+++ Count150614/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Count150614
+//
+//  Created by Ken Tominaga on 6/14/15.
+//  Copyright (c) 2015 Ken Tominaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
